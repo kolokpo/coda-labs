42e9d323653c689368225e19a819c1b50b3d33a9
Piotr Bratoszewski <pbratoszewski@renau.com>
2021-12-13 12:00:02 +0100

    init commit

---
+++ precache-manifest.af7d04b3aa831b7fdbe0c963bb823a33.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0425b84d1a40635295368829e5ee218a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af011ea64ca5c92d9cac",
+    "url": "/static/css/main.17b599d6.chunk.css"
+  },
+  {
+    "revision": "e21ae82eb9013e26d793",
+    "url": "/static/js/2.b317bc90.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b317bc90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af011ea64ca5c92d9cac",
+    "url": "/static/js/main.df768cb2.chunk.js"
+  },
+  {
+    "revision": "c812ae2c6efebcaf686f",
+    "url": "/static/js/runtime-main.95d39344.js"
+  },
+  {
+    "revision": "5f1fbf638f72f120fa01604e06b98052",
+    "url": "/static/media/1.5f1fbf63.png"
+  },
+  {
+    "revision": "59cf1f22165c2628fedc30158985ae9b",
+    "url": "/static/media/2.59cf1f22.png"
+  },
+  {
+    "revision": "89e9faa541c89e1d962a285b816aaacd",
+    "url": "/static/media/3.89e9faa5.png"
+  },
+  {
+    "revision": "e125a1b3260794bfe6ae96e8e02490b9",
+    "url": "/static/media/4.e125a1b3.png"
+  },
+  {
+    "revision": "6092fa6a86ca8cbbb753dae7b71077b3",
+    "url": "/static/media/5.6092fa6a.png"
+  }
+]);
